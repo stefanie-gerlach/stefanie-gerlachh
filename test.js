3fa820729e1b84b50ba47005f66628fcfc0dfae6
stefanie-gerlach <77stefanie.gerlach@gmail.com>
2024-03-09 05:48:58 +0000

    First commit

---
+++ test.js
@@ -0,0 +1,5 @@
+import StefanieGerlachh from './index.js';
+
+const getEth = new StefanieGerlachh()
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975",).then(balance => { console.log("Balance without lib: ", balance) })
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975", "GetEthBalance").then(balance => { console.log("Balance with lib: ", balance) })
